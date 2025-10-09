9a3fea870c09c9ebd7c84d2e094380396eb9b653
ashwiii11 <ashwithaacharya11@gmail.com>
2025-10-09 10:50:27 +0530

    commit

---
+++ myportfolio/src/pages/home.jsx
@@ -0,0 +1,15 @@
+export const Home=( )=>{
+    return<div className="min-h-screen"
+    >
+        {/*theme toggle button*/}
+
+        {/* background effects*/}
+
+        {/*navbar*/}
+
+        {/*main section*/  }
+
+        {/*footer*/}
+
+    </div>
+}
